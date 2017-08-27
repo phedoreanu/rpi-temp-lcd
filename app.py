059dab683bde26aadb474b6b81a93dfbaa1f9a7c
Adrian Fedoreanu <adrian.fedoreanu@gmail.com>
2017-08-27 17:58:43 +0100

    Add pytz

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from datetime import datetime
 
 import Adafruit_DHT
 import RPi.GPIO as GPIO
+import pytz as pytz
 from RPLCD import CharLCD
 
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23], numbering_mode=GPIO.BOARD)
 
 while True:
-    now = datetime.now()
+    now = datetime.now(pytz.timezone('Europe/London'))
     lcd.clear()
     lcd.cursor_pos = (0, 0)
 
@@ -26,5 +27,5 @@ while True:
 
         lcd.write_string(now.strftime('%b %d  %H:%M:%S\n'))
         lcd.cursor_pos = (1, 0)
-        lcd.write_string('IP {}'.format(ip))
+        lcd.write_string('IP: {}'.format(ip))
         time.sleep(1)
